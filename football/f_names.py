6b7d49bd6d1ef45f95c3e348dee0b976f799b324
Bobby Butler <bbutler3ky@gmail.com>
2019-12-15 19:23:26 -0500

    changes toward checking multi-character string through full name; edited to follow PEP 8

--- football/f_names.py
+++ football/f_names.py
@@ -1,14 +1,27 @@
 """
-12/15/2019
+Created 12/15/2019
 
-This script is designed to list all players on current NFL rosters with a certain
-letter in their last name (as specified in the "letter" variable near the top).
-Michael Cheiken proposed the idea when I asked him for suggestions of simple-ish
-sports projects that would teach me web-scraping.
+This script is designed to list all players on current NFL rosters with
+a certain letter in their last name (as specified in the "letter"
+variable near the top). Michael Cheiken proposed the idea when I asked
+him for suggestions of simple-ish sports projects that would teach me
+web-scraping.
 
-I used lineups.com partially because it was the first Google result when I 
-searched "nfl rosters" and partially because it claims to have live updates.
-This means we'll (theoretically) get the most up-to-date rosters at any given time.
+I used lineups.com partially because it was the first Google result
+when I searched "nfl rosters" and partially because it claims to have
+live updates. This means we'll (theoretically) get the most up-to-date
+rosters at any given time.
+
+Updated 12/15/2019 (lol)
+
+Cheiken wants any string of characters to be recognizable in a name.
+For last name alone, this can be done easily by replacing "letter"
+with "chars", signifying an expansion of the options for setting the
+variable. However, I'd also like to extend to the first name, *and*
+allow for crossover to last name. For example, consider the characters
+"ff". We would easily find this in "Jefferson". But we'd also like to
+identify it in "Sharif Finch". Now we need to eliminate whitespace for
+checking, and also check all capitalization permutations.
 """
 
 import time
@@ -18,58 +31,78 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
-# Specify letter to search last names for.
-letter = 'f'
+# Specify whether we're searching the last name or the full name.
+full = True
+# Specify letter to search names for.
+chars = 'f'
+# Specify whether to print numbers beside team names.
+rank = False
 
 # Main page url for the website we're scraping.
 stem = 'https://www.lineups.com'
 
-# Specify webpage where the links to NFL rosters live, and download its HTML code.
-# This is then parsed by BeautifulSoup and searched for roster links.
-# Note: the retrieved links are not full links. More below.
+# Specify webpage where the links to NFL rosters live, and download its
+# HTML code.  This is then parsed by BeautifulSoup and searched for
+# roster links.  Note: the retrieved links aren't full links. See below.
 rosters_url = stem + '/nfl/rosters/'
 rosters_html = urlopen(rosters_url)
-rosters_soup = BeautifulSoup(rosters_html,'lxml')
-link_chunks = rosters_soup.find_all('a', attrs={'href':re.compile('^/nfl/roster')})
+rosters_soup = BeautifulSoup(rosters_html, 'lxml')
+nfl_ros = '^/nfl/roster'
+link_chunks = rosters_soup.find_all('a', attrs={'href': re.compile(nfl_ros)})
 
-# Construct links to team rosters. This requires appending the main website url
-# to the beginning of each. 
+# Construct links to team rosters.  This requires appending the main
+# website url to the beginning of each.
 team_links = []
 i = 0
 for chunk in link_chunks:
-    # Each link on the page is duplicated for some reason. This is remedied here.
-    # Also, the first two links in the list are not team links, so they are removed.
+    # Each link on the page is duplicated for some reason.  This is
+    # remedied here.  Also, the first two links in the list are not team
+    # links, so they are removed.
     if (i % 2) == 0 and i > 1:
         link = chunk.get('href')
         team_links.append(link)
-    i+=1
+    i += 1
 
 # A list of suffixes for later removal from the last name.
-# This saves "Butler III" 
-suffixes = ['Jr', 'II', 'III', 'IV', 'V']
+# This saves (e.g.) "Butler III" from being identified as containing i.
+suffixes = ['Jr', 'Sr', 'II', 'III', 'IV', 'V']
 
 for link in team_links:
-    idx = team_links.index(link)
+    # Print team name, optionally with its rank 1-32 in alphabetical
+    # order by city.
     team_hyphen = link.split('/')[-1]
-    team_name = team_hyphen.replace('-',' ').title()
-    #print('%d. %s' % (idx+1, team_name))
-    print(team_name)
+    team_name = team_hyphen.replace('-', ' ').title()
+    if rank:
+        idx = team_links.index(link)
+        team_print = '%d. %s' % (idx+1, team_name)
+        spacing = ' '*5
+    else:
+        team_print = team_name
+        spacing = ' '*2
+    print(team_print)
+    # Construct individual roster url and retrieve roster with Pandas.
     team_url = stem + link
     roster = pd.read_html(team_url)[0]
-    for name in roster['Name']:
-        name_chunk = name.split('.')[0]
-        first_last = name_chunk.split('  ')[0]
-        name_list = first_last.split(' ')
-        full_name = ' '.join(name_list)
-        last_list = name_list[1:]
-        for suffix in suffixes:
-            if suffix in last_list:
-                last_list.remove(suffix)
-                if suffix=='Jr':
-                    full_name += '.'
-        last = ' '.join(last_list)
-        if (letter in last) or (letter.upper()) in last: 
-            print('  ' + full_name)
+    # Loop through each player and engineer their name from the mess
+    # returned in the dataframe.  Example 'Name' column entry for
+    # Leighton Vander Esch:
+    # 'Leighton Vander Esch  L. V. Esch'
+    for name_chunk in roster['Name']:
+        # Luckily, there are two spaces before the abbreviated name,
+        # so we can split on a double space to retrieve the full name.
+        full_name = name_chunk.split('  ')[0]
+        name_list = full_name.split(' ')
+        if full:
+            pass
+        else:
+            # Make last name into list (in case it contains > 1 word).
+            last_list = name_list[1:]
+            # Remove suffix from last name.
+            for suffix in suffixes:
+                if suffix in last_list:
+                    last_list.remove(suffix)
+            last = ' '.join(last_list)
+            if (chars in last) or (chars.title()) in last:
+                print(spacing + full_name)
     print()
     time.sleep(1)
-    
